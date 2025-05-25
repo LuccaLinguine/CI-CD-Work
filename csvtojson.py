01aff9b208402c69de691b23826b5a6babde7cd1
LuccaLinguine <lucaskesten11@gmail.com>
2025-05-25 19:16:02 +0200

    error when running tests - bad formatting of json file

--- csvtojson.py
+++ csvtojson.py
@@ -1,8 +1,8 @@
-import pandas
+import pandas as pd
 
-def csv_to_json(csv_file):
-    df = pandas.read_csv(csv_file)
-    df.to_json("profiles1.json", orient="records", force_ascii=False, lines=True)
+def csv_to_json(csv_file, json_file):
+    df = pd.read_csv(csv_file)
+    df.to_json(json_file, orient='records', indent=4, force_ascii=False)
 
-if __name__ == '__main__':
-    csv_to_json("profiles1.csv",)
+if __name__ == "__main__":
+    csv_to_json('profiles1.csv', 'profiles1.json')
